d2899ef9c1dae6cd1b742aaa13c887cf1298f09c
Yan <yanclopes08@gmail.com>
2023-11-24 18:11:37 -0300

    titulo finalizado

--- src/script.js
+++ src/script.js
@@ -9,3 +9,24 @@ function menuburguer(){
         menu.classList.add('d-none')
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var indice = 1;
+    const imagem1 = document.getElementById('foto1');
+    const imagem2 = document.getElementById('foto2');
+
+    function carrossel() {
+        if (indice % 2 === 0) {
+            imagem1.classList.add('d-none');
+            imagem2.classList.remove('d-none');
+        } else {
+            imagem1.classList.remove('d-none');
+            imagem2.classList.add('d-none');
+        }
+
+        indice++;
+        setTimeout(carrossel, 2000);
+    }
+
+    setTimeout(carrossel, 2000);
+});
